1723146102b675fc90575d9f4c5a11e8e191e78e
Henry Jones <henadinn+github223@gmail.com>
2026-03-24 11:22:12 +0100

    mv to lazy url

--- Places/PlaceModel.swift
+++ Places/PlaceModel.swift
@@ -4,7 +4,7 @@
 //
 //  Created by henry on 3/24/26.
 //
-
+import Foundation
 struct Locations: Decodable {
     let locations: [Place]
 }
@@ -13,4 +13,14 @@ struct Place: Decodable, Identifiable {
     let name: String?
     let lat: Double
     let long: Double
+    
+    lazy var url: URL? = {
+        guard var components = URLComponents(string: "wikipedia://places") else { return nil }
+        components.queryItems = [
+            URLQueryItem(name: "longitude", value: long.description),
+            URLQueryItem(name: "longitude", value: lat.description),
+            URLQueryItem(name: "name", value: name ?? "Untitled")
+        ]
+        return components.url
+    }()
 }
